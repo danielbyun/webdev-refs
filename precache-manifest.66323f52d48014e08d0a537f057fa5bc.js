3215f33d13834e3f8bae4459b7dc0ea380080757
danielbyun <dbyun92@gmail.com>
2020-08-13 16:44:24 +0900

    Updates

---
+++ precache-manifest.66323f52d48014e08d0a537f057fa5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa64c23cfe34de8e564b75997b365af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8236aad9616eed82eb2d",
+    "url": "/monsters-rolodex/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "10a030677462106c1bbd",
+    "url": "/monsters-rolodex/static/css/main.cabcdee7.chunk.css"
+  },
+  {
+    "revision": "8236aad9616eed82eb2d",
+    "url": "/monsters-rolodex/static/js/2.6fd9ed14.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/monsters-rolodex/static/js/2.6fd9ed14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a030677462106c1bbd",
+    "url": "/monsters-rolodex/static/js/main.2712cc16.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
