58a4fd155088022f9f22e10ae6ed893cf04bdd0c
danielbyun <dbyun92@gmail.com>
2020-08-24 23:24:17 +0900

    finish docker side, has bugs. need to know how to use docker with mysql

--- smartBrain-project/smart-brain-api/mysql/deploy_schemas.sql
+++ smartBrain-project/smart-brain-api/mysql/deploy_schemas.sql
@@ -1,4 +1,6 @@
 -- deploy fresh database tables
-i "/docker-entrypoint-initdb.d/tables/users.sql"
-i
-"/docker-entrypoint-initdb.d/tables/login.sql"
+\i
+'/docker-entrypoint-initdb.d/tables/users.sql'
+\i '/docker-entrypoint-initdb.d/tables/login.sql'
+
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
